49cc4d631de3256088ad7efdee9f517198f44b08
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-02-05 08:00:50 -0800

    2.50 algorithm design

---
+++ Chapter 2/2.50.js
@@ -0,0 +1,6 @@
+// Start at n
+// Create four arrays
+// each array entry contains cube of value up to n
+// do all pairwise additions of two of the arrays
+// do same for the second array
+// if there are equal sums, then find the roots (indices) of those pairs
