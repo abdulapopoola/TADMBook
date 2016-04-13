c592eb806398987d79197e38ae6e7b18cd280421
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-04-12 23:04:50 -0700

    iterative version

--- Chapter 3/3.23.js
+++ Chapter 3/3.23.js
@@ -2,3 +2,17 @@
     QUESTION
     Implement an algorithm to reverse a linked list. Now do it without recursion.
 */
+
+'use strict';
+function reverse(linkedList){
+    let node = linkedList;
+    let prev = null;
+    
+    while(node){
+        let tmp = node.next;
+        node.next = prev;
+        prev = node;
+        node = tmp;
+    }
+    return prev;
+}
