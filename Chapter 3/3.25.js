0056ea90595481f20146deb529ad6a334c5b5145
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-04-20 09:54:27 -0700

    solve 3.25

--- Chapter 3/3.25.js
+++ Chapter 3/3.25.js
@@ -4,3 +4,33 @@
      search string by cutting them out from the magazine. Give an algorithm to efficiently 
      determine whether the magazine contains all the letters in the search string.
 */
+
+function contains(searchStr, magazineChars) {
+    let charCodeToFreqMap = {};
+
+    let unfoundCharsLen = searchStr.length;
+    for (let i = 0; i < unfoundCharsLen; i++) {
+        let char = searchStr.charCodeAt(i);
+        if (charCodeToFreqMap[char]) {
+            charCodeToFreqMap[char]++;
+        } else {
+            charCodeToFreqMap[char] = 1;
+        }
+    }
+    
+    let magazineCharLen = magazineChars.length;
+    for (let i = 0; i < magazineCharLen; i++){
+        let char = magazineChars.charCodeAt(i);
+        if (charCodeToFreqMap[char] > 0){
+            unfoundCharsLen--;
+            charCodeToFreqMap[char]--;
+            if (unfoundCharsLen === 0){
+                return true;
+            }
+        }
+    }
+    
+    return false;
+}
+
+var test = contains('a boy', 'this is a boy string test');
